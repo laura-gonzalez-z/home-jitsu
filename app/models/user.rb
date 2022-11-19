6c2b7d5e7da74ac19e52288a05187653960c2121
Ricky Tran <diabloxtran@gmail.com>
2022-11-19 11:47:07 -0500

    added new fields to sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  BELTS = ["white", "blue", "purple", "brown", "black"].freeze
-  GENDERS = ["male", "female", "other"].freeze
+  BELTS = ["White", "Blue", "Purple", "Brown", "Black"].freeze
+  GENDERS = ["Male", "Female", "Other"].freeze
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -12,13 +12,9 @@ class User < ApplicationRecord
   validates :weight, length: { in: 2..4 }
   validates :height, presence: true
   validates :height, length: { in: 2..4 }
-  validates :belt, presence: true
   validates :belt, inclusion: { in: BELTS }
-  validates :years_of_experience, presence: true
   validates :years_of_experience, length: { in: 1..3 }
   validates :address, presence: true
-  validates :description, presence: true
   validates :gender, presence: true
   validates :gender, inclusion: { in: GENDERS }
-  validates :photo, presence: true
 end
