6fc133e505051689a123af58776b5b4c69e840d0
Ricky Tran <diabloxtran@gmail.com>
2022-11-22 21:06:09 -0500

    added edit/update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show]
+  before_action :set_user, only: %i[show edit update]
 
   def index
     @users = policy_scope(User)
@@ -9,9 +9,25 @@ class UsersController < ApplicationController
     authorize @user
   end
 
+  def edit
+    authorize @user
+    @message = "Edit your profile"
+  end
+
+  def update
+    authorize @user
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :weight, :height, :belt,
+                                 :years_of_experience, :address, :description, :gender)
   end
 end
