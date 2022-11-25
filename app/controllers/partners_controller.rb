6d0f94a00a07b98b48c8874b657deba1a6a40256
Ricky Tran <diabloxtran@gmail.com>
2022-11-24 19:39:28 -0500

    added partner creation

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,9 +1,17 @@
 class PartnersController < ApplicationController
-  def new
-    @partner = Partner.new
-  end
 
   def create
+    @partner = Partner.new(partner_params)
+    authorize @partner
+    @partner.save
+    redirect_to users_path
+  end
 
+  private
+
+  def partner_params
+    puts :requestee_id
+    puts :requester_id
+    params.permit(:requestee_id, :requester_id)
   end
 end
---
+++ app/policies/partner_policy.rb
@@ -0,0 +1,11 @@
+class PartnerPolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
