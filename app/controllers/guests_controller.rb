de1e19548f4d6a56a7d224ba7c62a5aed67a6e43
Jonathan Troupe <sanetroupe@gmail.com>
2022-11-26 16:47:53 -0500

    Adding ability to join an event through the guests list

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,2 +1,20 @@
 class GuestsController < ApplicationController
+  before_action :set_event, only: %i[create]
+
+  def create
+    @guest = Guest.new
+    @guest.event_id = @event.id
+    @guest.guest_id = current_user.id
+    if @guest.save
+      redirect_to events_path
+    else
+      render event_path(@event), :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
 end
