132fc902f119d0034e9a8b68485e567d10ee1712
Jonathan Troupe <sanetroupe@gmail.com>
2022-11-26 13:49:47 -0500

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_26_180734) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-ActiveRecord::Schema[7.0].define(version: 2022_11_26_161450) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  end
 
   create_table "chatrooms", force: :cascade do |t|
     t.string "name"
