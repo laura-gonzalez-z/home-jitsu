9511896794ec3b7be6ef2a3bb979d56e6ebc90cf
Jonathan Troupe <sanetroupe@gmail.com>
2022-11-26 16:18:52 -0500

    Adding a page to view all of my events

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,19 @@ Rails.application.routes.draw do
   resources :users do
     resources :reviews
   end
+
+  get "my_events", to: "events#my_events"
+
   resources :events
+
   resources :partners
   resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
+
   root to: "pages#home"
   get "my_messages", to: "chatrooms#my_messages"
   patch "/partners/:id/accept", to: "partners#accept", as: :accept
   patch "/partners/:id/reject", to: "partners#reject", as: :reject
+
 end
