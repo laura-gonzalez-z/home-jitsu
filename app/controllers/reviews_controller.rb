dd1a0bc8715d6f6bb8b1ea800e939bc0b4578a0b
Ricky Tran <diabloxtran@gmail.com>
2022-11-27 11:13:33 -0500

    added back button to reviews page for user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_user, only: %i[new create]
+  before_action :set_user, only: %i[index new create]
 
   def index
     @users = User.all
