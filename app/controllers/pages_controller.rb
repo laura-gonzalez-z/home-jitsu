f9ba0403f2571c767cb90e9980f21c93fa2615e7
Laura Gonzalez <lauragonzalezz@outlook.com>
2022-11-29 22:15:43 -0500

    Stars partially working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    if user_signed_in?
+      @average_rating = []
+      @average_rating = current_user.reviews.average(:rating).round(2) if current_user.reviews.exists?
+    else
+
+    end
   end
 end
