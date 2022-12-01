85b252a3ef0aa1debf18edc2a339d5ef97ab30f5
Laura Gonzalez <lauragonzalezz@outlook.com>
2022-12-01 14:05:37 -0500

    Users and user cards somewhat working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,12 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update]
 
   def index
-    @users = policy_scope(User)
+    if params[:query].present?
+      sql_query = "first_name ILIKE :query OR last_name ILIKE :query"
+      @users = policy_scope(User).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @users = policy_scope(User)
+    end
   end
 
   def show
